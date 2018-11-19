1fbee5e1f98ee7398e8d0ff490ea7467bc114986
jsnjfndsfdsasd <40805903+ujhsfjhfsfa@users.noreply.github.com>
2018-11-19 23:49:07 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -16,7 +16,18 @@ let done = {};
 
 
 
-
+const games = JSON.parse(fs.readFileSync('./games.json', "utf8"));
+client.on("message", message => {
+  if (message.author.bot) return;
+  if (!message.channel.guild) return;
+  if (!games[message.author.id]) games[message.author.id] = {
+    credits: 0,
+    level: 1,
+  };
+fs.writeFile('./games.json', JSON.stringify(games), (err) => {
+if (err) console.error(err);
+});
+});
 
 client.on("message", (message) => {
   var sender = message.author
